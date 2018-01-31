fb7aa36e7f3b52523dab497559e2ae17f8509e1e
LeeSD90 <lee.doyle90@gmail.com>
2018-01-31 10:53:01 +0100

    Added some inputs

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ function add(num1, num2){
 	return num1 + num2;
 }
 
-function subtract(){
-
+function subtract(num1, num2){
+	return num1 - num2;
 }
 
-function multiply(){
-
+function multiply(num1, num2){
+	return num1 * num2;
 }
 
-function divide(){
-	
+function divide(num1, num2){
+	return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+	switch(operator){
+		case '+':
+			return add(num1, num2);
+		case '-':
+			return subtract(num1, num2);
+		case '*':
+			return multiply(num1, num2);
+		case '/':
+			return divide(num1, num2);
+	}
 }
