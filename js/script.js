194305b7583d6f288d077b00cf9c1c3eb05cf380
LeeSD90 <lee.doyle90@gmail.com>
2018-02-05 15:21:38 +0100

    Wired up most of the UI to their JS functions and added some clears for the screen when inputting operations.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let displayValue = document.getElementById('display');
+let buttons = document.getElementById("buttons");
+let storedValue;
+let operator;
+let total;
+
 function add(num1, num2){
 	return num1 + num2;
 }
@@ -26,3 +32,42 @@ function operate(operator, num1, num2){
 			return divide(num1, num2);
 	}
 }
+
+function updateDisplay(value){
+	document.getElementById('display').innerHTML += value;
+}
+
+function clear(){
+	displayValue.innerHTML = 0;
+}
+
+function buttonPressed(e){
+	let button = e.target.value;
+
+	if(button === 'Clear'){
+
+			clear();
+
+	}
+	else if(button === '+' || 
+			button === '-' ||
+			button === '*' ||
+			button === '/'){
+		displayValue.innerHTML = button;
+		storedValue = displayValue.innerHTML;
+		operator = button;
+
+	} else if(button === '='){
+
+		operate(operator, storedValue, displayValue.innerHTML);
+
+	} else{
+
+		if(displayValue.innerHTML === "0" || isNaN(displayValue.innerHTML)) { displayValue.innerHTML = ""; }
+		displayValue.innerHTML += button;
+
+	}
+
+}
+
+buttons.addEventListener("click", buttonPressed);
